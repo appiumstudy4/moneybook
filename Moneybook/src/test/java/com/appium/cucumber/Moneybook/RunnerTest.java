9f41798ec0974c11d3b6a06d86f2a2b2c8c3fe67
Hanzisu <jisu.han@nhn.com>
2015-12-04 12:52:50 +0900

    cucumber report format add

--- Moneybook/src/test/java/com/appium/cucumber/Moneybook/RunnerTest.java
+++ Moneybook/src/test/java/com/appium/cucumber/Moneybook/RunnerTest.java
@@ -2,12 +2,13 @@ package com.appium.cucumber.Moneybook;
 
 import org.junit.runner.RunWith;
 
+
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "src/test/resource")
+		format = {"json:target/cucumber.json"},features = "src/test/resource")
 
 public class RunnerTest {
 
